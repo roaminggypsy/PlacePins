b3997f1e7518b56438cad8cf44f8a2c64cd29f56
Linjun Ouyang <lynjunoyang@gmail.com>
2021-01-12 22:52:33 -0800

    setup google login

--- server.js
+++ server.js
@@ -4,10 +4,24 @@ const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const { findOrCreateUser } = require('./controllers/userController');
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: async ({ req }) => {
+    let authToken = null;
+    let currentUser = null;
+    try {
+      authToken = req.headers.authorization;
+      if (authToken) {
+        currentUser = await findOrCreateUser(authToken);
+      }
+    } catch (err) {
+      console.error(`Unable to authenticate user with token ${authToken} `);
+    }
+    return { currentUser };
+  },
 });
 
 mongoose
